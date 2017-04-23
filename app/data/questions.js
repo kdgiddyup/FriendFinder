422c0b751606350d82f57396cd6891084e008f09
Kelly Davis <kellydavis1974@gmail.com>
2017-04-22 21:21:54 -0400

    working on survey form validation and processing

---
+++ app/data/questions.js
@@ -0,0 +1,38 @@
+  	var questions = [
+        {
+            question: 'Do you like pets?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Cats or dogs?',
+            options: ['1 (Cats)','2','3','4','5 (Dogs'] 
+      },
+        {
+            question: 'Do you want children?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Are you an outdoors person?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Are you good with a Netflix binge?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Do you attend a church?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Do you read a lot?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      },
+        {
+            question: 'Where do you land on the political spectrum?',
+            options: ['1 (Liberal)','2','3','4','5 (Conservative'] 
+      },
+        {
+            question: 'Are you happy in a crowd?',
+            options: ['1 (Strongly Agree)','2','3','4','5 (Strongly Disagree'] 
+      }
+      ];
