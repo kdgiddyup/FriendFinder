e112a66f290dbeddc20c57fad4a400daa9465c0b
Kelly Davis <kellydavis1974@gmail.com>
2017-04-24 15:51:46 -0400

    modified api/friends endpoint to return html instead of raw JSON in order to include link back to home; used JSON stringify with pretty-print options, wrapped in <pre> tags, to present JSON on html page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -36,8 +36,9 @@ apiRouter.route("/api/friends")
             });
         function returnJSON(snapshot) {
             var members = snapshot.val().members;
-            // since Firebase is already returning the JSON representation of the membership, we can just ship it back out to the client
-            res.json(members);
+            // Since Firebase is already returning the JSON representation of the membership, we can almost just ship it back out to the client.
+            // However, if we want a link back to the homepage, we are res.send()-ing it, not res.json()-ing it, therefore we HTML-ize it with the pretty-print formatter wrapped in <pre></pre> tags. <-- Hey, thanks, stackoverflow
+            res.send('<h2><a href="/">Return home</a></h2>'+'<pre>'+JSON.stringify(members, null, 2)+'</pre>');
             }
         })
     .post(function(req, res) {
@@ -55,29 +56,34 @@ apiRouter.route("/api/friends")
             var totalDifference = 0;
             // loop through entire current membership
             for (var i = 0; i< members.length; i++){
-                // for each member, loop through this member's score array
-                // zero out totalDifference
+                // zero out totalDifference for this member
                 totalDifference = 0;
+
+                // for this member, loop through the score array
                 for (var scoreIndex = 0; scoreIndex < members[i].scores.length; scoreIndex++) {
-                    // only parse to integers and compute differences if array values are not equal
+                    // only compute differences if array values between incoming user and this member are not equal
                     if (members[i].scores[scoreIndex] != incomingScores[scoreIndex]) {
                         // parse array values to integers, get difference and apply Math.abs() to eliminate negative sign, and add to this cycle's totalDifference value
-                        totalDifference += Math.abs(parseInt(incomingScores[scoreIndex])-parseInt(members[i].scores[scoreIndex]));                 }
+                        totalDifference += Math.abs(parseInt(incomingScores[scoreIndex])-parseInt(members[i].scores[scoreIndex]));                 
+                    }
                 } // end this member's scores loop
-                // push the ID of this member and the difference value to the differenceScores array
+                // push the ID of this member and the totalDifference value to the differenceScores array; we need to include the ID because we'll modify the array to find the closest match
                 differenceScores.push({id:i,score:totalDifference});
-            } // end membership for loop
-            // sort the array of all scores to find the minimum
+            } // end membership FOR loop
+            // we now have an array of {id,differenceScores} ojects we can sort to find the minimum differenceScore, which is our best match
             var bestMatch = differenceScores.sort(function(a, b){
                 return a.score-b.score
-            })[0];
+            })[0];  // [0] =  first index in the sorted array, which is sorted smallest difference to largest
 
-            // add user info to Firebase DB; they're now members, too.
-            firebase.database().ref("members/"+(members.length)).update( incomingUser );
+            // now that we're done messing with existing members, add our new user info to Firebase DB; they're now members, too.
+            // a future improvement would be to evaluate member name against existing members and get user input on if they want to overwrite or change name to unique value
+            firebase.database().ref("members/"+(members.length)).update( incomingUser ); 
+            // notes about above firebase notation: ref("members/") sets path in this Firebase DB project; +(members.length) adds a new key for this member equal to the length of the members object, which is to say it increments up by one 
             
             // finally, respond in JSON with name and image of best match:
             res.json({name: members[bestMatch.id].name,image: members[bestMatch.id].photo});
         }  // end evaluateUser function
 });
 
+// make this route object available to server.js
 module.exports = apiRouter;
